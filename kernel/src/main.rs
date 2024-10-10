702095681990b780491dd00049e409a191dac7d9
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-10-10 19:51:00 +1100

    Create empty kernel
    For now, BIOS building does not work, so it was removed from the template

---
+++ kernel/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+bootloader_api::entry_point!(kernel_main);
+fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    loop {
+
+    }
+}
