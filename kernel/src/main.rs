a87210780ee41824648f235a48e1d277e29858c5
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-10-10 20:09:08 +1100

    Add rainbow screen to test framebuffer

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -12,7 +12,17 @@ fn panic(_info: &PanicInfo) -> ! {
 
 bootloader_api::entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    loop {
+    let mut framebuffer = boot_info.framebuffer.take().unwrap();
+    let info = framebuffer.info().clone();
+    let mut buffer = framebuffer.buffer_mut();
 
+    for y in 0..info.height {
+        for x in 0..info.width {
+            buffer[(y * info.stride + x) * info.bytes_per_pixel] = ((x as f32 / info.width as f32) * 255f32) as u8; // blue
+            buffer[(y * info.stride + x) * info.bytes_per_pixel + 1] = ((y as f32 / info.height as f32) * 255f32) as u8; // green
+            buffer[(y * info.stride + x) * info.bytes_per_pixel + 2] = 128; // red
+        }
     }
+
+    loop {}
 }
