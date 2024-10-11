724527bd9f323906383f0aad4a05cf17d4ef550b
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-10-11 18:40:20 +1100

    Clean up: Remove redundant variable

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ fn main() {
     // read env variables that were set in build script
     let uefi_path = env!("UEFI_PATH");
 
-    // choose whether to start the UEFI or BIOS image
-    let uefi = true;
-
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
     cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
     cmd.arg("-drive").arg(format!("format=raw,file={uefi_path}"));
