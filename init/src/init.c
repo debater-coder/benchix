2f4844c8799970a5fbacb15472125274cb60bf59
debater-coder <hamzah@syedahmed.net>
2025-07-10 22:04:29 +1000

    feat: libc hello world

--- init/src/init.c
+++ init/src/init.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int main() {
+    open("/dev/console", O_RDONLY); // fd 0 -- stdin
+    open("/dev/console", O_WRONLY); // fd 1 -- stdout
+    open("/dev/console", O_WRONLY); // fd 2 -- stderr
+
+    write(1, "Hello, World!\n", 14);
     return 42;
 }
